d7ee131aa5344d160f410e97c8fd57c8e47b8bab
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-12 15:23:49 -0700

    commit tree bf98c4073b01e294516b749fd8191c0b5e1a14bd

--- index.js
+++ index.js
@@ -1,1 +1,89 @@
-// Write your solution here!
+// // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){ 
+  return cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(name) {
+  return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+  return cats.shift()
+}
+
+function appendCat(name) {
+   const copyOfCats = [...cats]
+   copyOfCats.push('Broom')
+   return copyOfCats
+}
+
+function prependCat(name) {
+  const copyOfCats = [...cats]
+  copyOfCats.unshift('Arnold')
+  return copyOfCats
+}
+
+function removeLastCat(name) {
+  const copyOfCats = [...cats]
+  copyOfCats.pop()
+  return copyOfCats
+}
+
+function removeFirstCat(name) {
+  const copyOfCats = [...cats]
+  copyOfCats.shift()
+  return copyOfCats
+}
+
+
+
+
+
+
+// const cats = ["Milo",  "Otis", "Garfield"]
+
+// function destructivelyAppendCat(name){
+//   return cats.push("Ralph");
+// }
+
+// function destructivelyPrependCat(name){
+//   return cats.unshift("Bob");
+// }
+
+// function destructivelyRemoveLastCat(name){
+//   return cats.pop();
+// }
+
+// function destructivelyRemoveFirstCat(name){
+//   return cats.shift();
+// }
+
+// function appendCat(name){
+//   const copyOfCats = cats.slice()
+//   copyOfCats.push("Broom")
+//   return copyOfCats;
+// }
+
+// function prependCat(name){
+//   const copyOfCats = cats.slice()
+//   copyOfCats.unshift("Arnold")
+//   return copyOfCats;
+// }
+
+// function removeLastCat(name){
+//   const copyOfCats = cats.slice()
+//   copyOfCats.pop()
+//   return copyOfCats;
+// }
+
+// function removeFirstCat(name){
+//   const copyOfCats = cats.slice()
+//   copyOfCats.shift()
+//   return copyOfCats;
+// }
